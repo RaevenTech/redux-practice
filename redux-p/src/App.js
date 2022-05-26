726b7f2647bba505d43fdaee169727cfade61661
RaevenTech <100418770+RaevenTech@users.noreply.github.com>
2022-05-26 12:02:25 +0100

    h1 added

--- redux-p/src/App.js
+++ redux-p/src/App.js
@@ -3,6 +3,8 @@ import Counter from "./features/counter/Counter";
 function App() {
     return (
         <div className="App">
+            <h1> Counter </h1>
+            <hr />
             <Counter />
         </div>
     );
